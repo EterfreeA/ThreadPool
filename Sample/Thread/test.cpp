e2717de5e6afc136ffd839a2066aa0af7de81a59
solifree <solifree@qq.com>
2023-11-26 02:58:58 +0800

    update v4.0.0

--- Sample/Thread/test.cpp
+++ Sample/Thread/test.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	using std::cout, std::boolalpha, std::endl;
 
-	Eterfree::Concurrency::Thread thread;
+	ETERFREE_SPACE::Concurrency::Thread thread;
 	cout << thread.getID() << endl;
 
 	thread.configure([] \
